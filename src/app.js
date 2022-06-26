632fde81927ee7126d0d1a10f37bd49f275e5d20
caiosev <kaiosev9@gmail.com>
2022-06-26 15:39:33 -0300

    removing prelight

--- src/app.js
+++ src/app.js
@@ -31,11 +31,8 @@ class App {
 
   middlewares() {
     this.app.use(cors({
-      allowedHeaders: ['sessionId', 'Content-Type'],
-      exposedHeaders: ['sessionId'],
       origin: '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
     }));
     this.app.use(helmet());
     this.app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
