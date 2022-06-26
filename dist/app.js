037b1226673ad963b15c22b6aaac2966550b1292
caiosev <kaiosev9@gmail.com>
2022-06-26 15:21:23 -0300

    policy change

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@
 
 var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _path = require('path');
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
@@ -21,11 +22,6 @@ require('./database');
 
 _dotenv2.default.config();
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, );
@@ -34,7 +30,9 @@ class App {
   }
 
   middlewares() {
-    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_cors2.default.call(void 0, ));
+    this.app.use(_helmet2.default.call(void 0, ));
+    this.app.use(_helmet2.default.crossOriginResourcePolicy({ policy: 'cross-origin' }));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
