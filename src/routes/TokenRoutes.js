e7ec4daff88e46486a6324a7f30771d8e6dc687f
caiosev <kaiosev9@gmail.com>
2022-06-27 13:42:06 -0300

    cors em token

--- src/routes/TokenRoutes.js
+++ src/routes/TokenRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import cors from 'cors';
 import TokenController from '../controllers/TokenController';
 
 const router = new Router();
 
-router.post('/', TokenController.store);
+router.post('/', cors(), TokenController.store);
 
 export default router;
