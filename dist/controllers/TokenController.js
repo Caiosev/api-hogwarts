d5eede43c79b9d8d08e316b2060d5e8bc5f116c3
caiosev <kaiosev9@gmail.com>
2022-06-28 11:59:03 -0300

    build com token retornando id

--- dist/controllers/TokenController.js
+++ dist/controllers/TokenController.js
@@ -28,7 +28,7 @@ class TokenController {
     const token = _jsonwebtoken2.default.sign({ id, login }, process.env.TOKEN_SECRET, {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
-    return res.json({ token, user: { nome: prof.nome, login } });
+    return res.json({ token, user: prof });
   }
 }
 exports. default = new TokenController();
